0d61f51d4cf145e624e0411f41d0dd6ff239fe23
github username <github email address>
2019-12-29 23:41:41 -0600

    source file and complied exe

---
+++ simpleNotesSHOES.rb
@@ -0,0 +1,40 @@
+require 'yaml/store'
+
+Shoes.app height: 200, width: 200, title: "Simple Notes" do
+
+  flow do
+
+    button "New" do
+      store = YAML::Store.new "config.yml"
+      store.transaction(true) do
+        @title.text = store['title']
+        @para.text = store['para']
+      end
+    end
+
+    button "Open" do
+      file = ask_open_file
+      store = YAML::Store.new file
+      store.transaction(true) do
+        @title.text = store['title']
+        @para.text = store['para']
+      end
+    end
+
+    button "Save" do
+      file = ask_save_file
+
+      store = YAML::Store.new file
+
+      store.transaction do
+        store["title"] = @title.text
+        store["para"] = @para.text
+      end
+    end
+
+    flow do
+      @title = edit_line "New note", margin: [0, 5, 0, 5]
+      @para = edit_box "Describe your note here"
+    end
+  end
+end
